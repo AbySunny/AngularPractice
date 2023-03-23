09490abe96b80623b514c5a9f7bf6d7a9a3c15df
Aby Sunny <abysunny2001@gmail.com>
2023-03-23 11:25:13 +0530

    third commmit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-login',
@@ -9,49 +11,25 @@ export class LoginComponent  implements OnInit{
 
   data="Your Perfect Banking Partner"
   inputplacsholder="Account Number"
-  // acno=''
-  // // or acno:any
-  // psw=''
+  acno=''
+  psw=''
 
-  userDetails:any={
-    1000:{acno:1000,username:"anu",password:"abc123",balance:"0"},
-    1001:{acno:1001,username:"amal",password:"abc123",balance:"0"},
-    1002:{acno:1002,username:"arun",password:"abc123",balance:"0"},
-    1003:{acno:1003,username:"akil",password:"abc123",balance:"0"}
-  }
+  
 
-  constructor() { }
+  constructor(private router:Router,private ds:DataService) { }
 
   ngOnInit(): void {
     
   }
 
-  // login(){
-  //   var acnum=this.acno
-  //   var psw=this.psw
-  //   var userDetails1=this.userDetails
-  //   if(acnum in userDetails1){
-  //     if(psw==userDetails1[acnum]["password"]){
-  //       alert("login success")
-  //     }
-  //     else{
-  //       alert("incorrect password")
-  //     }
-  //   }
-  //   else{
-  //     alert('acno incorrect or not registered yet')
-  //   }
-  // }
-
-
-  login(a:any,b:any){
-
-    var acnum=a.value
-    var psw=b.value
-    var userDetails1=this.userDetails
+  login(){
+    var acnum=this.acno
+    var psw=this.psw
+    var userDetails1=this.ds.userDetails
     if(acnum in userDetails1){
       if(psw==userDetails1[acnum]["password"]){
         alert("login success")
+        this.router.navigateByUrl("dashboard")
       }
       else{
         alert("incorrect password")
@@ -63,17 +41,5 @@ export class LoginComponent  implements OnInit{
   }
 
 
-
-  // acnoChange(event:any){
-  //   this.acno=event.target.value
-  //   // console.log(this.acno);
-    
-
-  // }
-  // psdChange(event:any){
-  //   this.psw=event.target.value
-  //   // console.log(this.psw);
-    
-  // }
-
+  
 }
